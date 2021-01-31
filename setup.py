f65fb3c560bd93e0656003a41c7f65916f24bb8f
Surya Sankar <suryashankar.m@gmail.com>
2021-02-01 03:30:07 +0530

    upgrading version number

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/suryasankar/databuddy',
-    version='0.1.14',
+    version='0.1.15',
     zip_safe=False,
 )
